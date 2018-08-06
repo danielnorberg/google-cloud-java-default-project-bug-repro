803d34916df74ffc4a93afd4769ea20b9615321e
Daniel Norberg <dano@spotify.com>
2018-08-06 16:12:08 +0200

    print credentials

--- src/main/java/Repro.java
+++ src/main/java/Repro.java
@@ -1,8 +1,11 @@
+import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.ServiceOptions;
+import java.io.IOException;
 
 public class Repro {
 
-  public static void main(String[] args) {
-    System.err.println("ServiceOptions.getDefaultProjectId(): " + ServiceOptions.getDefaultProjectId());
+  public static void main(String[] args) throws IOException {
+    System.out.println("ServiceOptions.getDefaultProjectId(): " + ServiceOptions.getDefaultProjectId());
+    System.out.println("GoogleCredentials.getApplicationDefault(): " + GoogleCredentials.getApplicationDefault());
   }
 }
